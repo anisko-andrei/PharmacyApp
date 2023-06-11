1c2b268c4be6f8b066fcdd63ce7685084c034ff3
aniskovich <anisko.andrei@gmail.com>
2023-06-11 12:57:50 +0300

    Initial Commit

---
+++ Pharmacy/PharmacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PharmacyApp.swift
+//  Pharmacy
+//
+//  Created by Андрей Аниськович on 11.06.23.
+//
+
+import SwiftUI
+
+@main
+struct PharmacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
