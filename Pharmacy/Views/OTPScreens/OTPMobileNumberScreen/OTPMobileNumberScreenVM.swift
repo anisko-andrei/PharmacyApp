17ffe04096948ae4c53e92a35aa517e5aef6387b
aniskovich <anisko.andrei@gmail.com>
2023-06-20 13:11:30 +0300

    saveIT

--- Pharmacy/Views/OTPScreens/OTPMobileNumberScreen/OTPMobileNumberScreenVM.swift
+++ Pharmacy/Views/OTPScreens/OTPMobileNumberScreen/OTPMobileNumberScreenVM.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Alamofire
 
 final class OTPMobileNumberScreenVM: ObservableObject {
     
@@ -20,11 +21,40 @@ final class OTPMobileNumberScreenVM: ObservableObject {
         }
     }
     
+    func sendPhone(phone: String) async throws -> Msg {
+        try await withCheckedThrowingContinuation { continuation in
+            AF.request("http://localhost:3000/api/v1/auth/phoneNumber/login", method: .post, parameters: ["phoneNumber": phone],encoding: JSONEncoding.default).responseDecodable(of: Msg.self, completionHandler:  { result in
+                switch result.result {
+                case .success(let data):
+                    continuation.resume(returning: data)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            })
+        }
+    }
+        
+    
+    
     func getOTPCode() {
         let mobile =  mobileNumberText.replacingOccurrences(of: "-", with: "").replacingOccurrences(of: " ", with: "")
         if mobile.count == 13 {
             print(mobile)
-            showCodeScreen.toggle()
+            
+            Task{
+                do{
+                let sendOTPStatus = try await sendPhone(phone: mobile)
+                    print(sendOTPStatus)
+                    await MainActor.run(body: {
+                        showCodeScreen.toggle()
+                    })
+                }
+                catch {
+                    await MainActor.run(body: {
+                        alertBody = AppAlert(message: String(localized: "no register"))
+                    })
+                }
+            }
         }
         else {
             alertBody = AppAlert(message: String(localized: "Invalid phone number"))
@@ -35,4 +65,19 @@ final class OTPMobileNumberScreenVM: ObservableObject {
 struct AppAlert : Identifiable {
     var id: String {message}
     var message: String
+}
+
+
+// MARK: - Welcome
+struct Welcome: Decodable {
+    let customer: Customer
+}
+
+// MARK: - Customer
+struct Customer: Decodable {
+    let firstName, lastName: String
+}
+
+struct Msg: Decodable {
+    let message: String
 }
